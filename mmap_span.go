3b05fbc3017aa34c5f85ccb30bfa7a8f32056654
cto@hbwhale.com <cto@hbwhale.com>
2016-11-01 15:10:55 +0900

    - ver 1

---
+++ mmap_span.go
@@ -0,0 +1,63 @@
+package torrent
+
+import (
+	"io"
+
+	"github.com/tysontate/gommap"
+)
+
+type MMap struct {
+	gommap.MMap
+}
+
+func (me MMap) Size() int64 {
+	return int64(len(me.MMap))
+}
+
+type MMapSpan []MMap
+
+func (me MMapSpan) span() (s span) {
+	for _, mmap := range me {
+		s = append(s, mmap)
+	}
+	return
+}
+
+func (me MMapSpan) Close() {
+	for _, mMap := range me {
+		mMap.UnsafeUnmap()
+	}
+}
+
+func (me MMapSpan) Size() (ret int64) {
+	for _, mmap := range me {
+		ret += mmap.Size()
+	}
+	return
+}
+
+func (me MMapSpan) ReadAt(p []byte, off int64) (n int, err error) {
+	me.span().ApplyTo(off, func(intervalOffset int64, interval sizer) (stop bool) {
+		_n := copy(p, interval.(MMap).MMap[intervalOffset:])
+		p = p[_n:]
+		n += _n
+		return len(p) == 0
+	})
+	if len(p) != 0 {
+		err = io.EOF
+	}
+	return
+}
+
+func (me MMapSpan) WriteAt(p []byte, off int64) (n int, err error) {
+	me.span().ApplyTo(off, func(iOff int64, i sizer) (stop bool) {
+		_n := copy(i.(MMap).MMap[iOff:], p)
+		p = p[_n:]
+		n += _n
+		return len(p) == 0
+	})
+	if len(p) != 0 {
+		err = io.EOF
+	}
+	return
+}
